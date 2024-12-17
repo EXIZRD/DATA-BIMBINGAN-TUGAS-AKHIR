2d9642dd8dafe29a7cfe04e812721d63ed5cb336
Zainul-Alif <zainulalif@student.telkomuniversity.ac.id>
2024-12-17 13:25:04 +0700

    Update TB.h

--- TB.h
+++ TB.h
@@ -4,14 +4,6 @@
 
 using namespace std;
 
-typedef struct mahasiswa infotypeM;
-
-struct mahasiswa {
-    string nama;
-    int NIM;
-    string Kelas;
-};
-
 typedef struct dosen infotypeD;
 
 struct dosen {
@@ -20,15 +12,7 @@ struct dosen {
     string Matkul;
 };
 
-typedef struct elmRelasi *adrRelasi;
-typedef struct elmMhs *adrMhs;
 typedef struct elmDsn *adrDsn;
-
-struct elmMhs{
-    infotypeM info;
-    adrRelasi firstR;
-    adrMhs next;
-};
 
 struct elmDsn{
     infotypeD info;
@@ -36,49 +20,82 @@ struct elmDsn{
     adrDsn prev;
 };
 
-struct elmRelasi{
-    adrDsn infoD;
-    adrRelasi next;
-    adrMhs infoM;
-};
-
-struct listMhs{
-    adrMhs First;
-};
 
 struct listDsn{
     adrDsn first;
     adrDsn last;
 };
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+typedef struct mahasiswa infotypeM;
+
+struct mahasiswa {
+    string nama;
+    int NIM;
+    string Kelas;
+};
+
+typedef struct elmMhs *adrMhs;
+
+struct elmMhs{
+    infotypeM info;
+    adrMhs next;
+};
+
+struct listMhs{
+    adrMhs First;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+typedef struct elmRelasi *adrRelasi;
+
+struct elmRelasi{
+    adrDsn infoD;
+    adrRelasi next;
+    adrMhs infoM;
+};
+
 struct listRelasi{
     adrRelasi first;
 };
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void createListMhs(listMhs &M);
-void createElmMhs(string nama, int NIM, string Kelas);
-bool isEmptyMhs(listMhs M);
-void insertfirstM(listMhs &M, adrMhs P);
-adrMhs FindMhs(listMhs M, int NIM);
-void deleteFirstMhs(listMhs &M);
-void deleteLastMhs(listMhs &M);
-void printOneDataMhs(listMhs M, int NIM);
-void printAllDataMhs(listMhs M);
-
-void DeleteRlsMhs(adrMhs M);
-void DeleteRlsDsn(adrDsn D, listMhs &M);
-void Relasi(listMhs &M, listDsn &D, int NIM, string Kode);
 
 void createListDsn(listDsn &D);
-void createElmDsn(string nama,string kode, string matkul);
+adrDsn createElmDsn(string nama,string kode, string matkul);
 bool isEmptyDsn(listDsn D);
-void insertlast(listDsn &D, adrDsn P);
+void insertLastD(listDsn &D, adrDsn P);
 adrDsn FindDsn(listDsn D, string Kode);
 void deleteFirstDsn(listDsn &D, listMhs &M);
 void deleteLastDsn(listDsn &D, listMhs &M);
 void printOneDataDsn(listDsn D, string kode);
 void printAllDataDsn(listDsn D, listMhs M);
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void createListMhs(listMhs &M);
+adrMhs createElmMhs(string nama, int NIM, string Kelas);
+bool isEmptyMhs(listMhs M);
+void insertFirstM(listMhs &M, adrMhs P);
+adrMhs FindMhs(listMhs M, int NIM);
+void deleteFirstMhs(listMhs &M);
+void deleteLastMhs(listMhs &M);
+void printOneDataMhs(listMhs M, listRelasi &R, int NIM);
+void printAllDataMhs(listMhs M, listRelasi &R);
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void createListRls(listRelasi &R);
+adrRelasi createElmRls(adrDsn d, adrMhs m);
+void insertFirstR(listRelasi &R, adrRelasi p);
+void connect(listRelasi &R, listDsn D, listMhs M, string KodeDsn, int NIM);
+adrRelasi findRelasiMhs(listRelasi &R, int NIM);
+adrRelasi findRelasiDosen(listRelasi &R, string kodeDosen);
+void DeleteRlsMhs(listRelasi &R, adrMhs m);
+void DeleteRlsDsn(adrDsn D, listRelasi &R);
 
 
 #endif // TB_H_INCLUDED
